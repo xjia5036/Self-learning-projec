be8741968c34324cdaa1e4c33357cdf2a14d43db
xjia5036 <jxu185@myseneca.ca>
2024-12-07 16:26:34 -0500

    commit

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -100,19 +100,19 @@ function App() {
   );
 }
 
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+// import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <YourComponent />
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/" exact>
+//           <YourComponent />
+//         </Route>
+//       </Switch>
+//     </Router>
+//   );
+// }
 
 
 export default App;
